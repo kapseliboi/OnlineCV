94fe2a2eb54d30f65bc68bd612634d508c893ab4
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-09 22:32:54 +0200

    Added redux provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
 
+import store from "./store";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
@@ -9,12 +11,16 @@ import Register from "./components/auth/Register";
 class App extends Component {
   render() {
     return (
-      <Router>
-      <div className="App">
-      <Register />
-      <Landing />
-      </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/" component={Landing} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
