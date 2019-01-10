1ac542068d6673b6d28536275802b232fd8d28d9
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-10 23:44:08 +0200

    Removed password validation since passwords are generated SSR

--- validation/register.js
+++ validation/register.js
@@ -6,19 +6,12 @@ module.exports = function validateRegisterInput(data) {
 
   data.name = !isEmpty(data.name) ? data.name : "";
   data.username = !isEmpty(data.username) ? data.username : "";
-  data.password = !isEmpty(data.password) ? data.password : "";
 
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
   }
   if (Validator.isEmpty(data.username)) {
     errors.username = "Username field is required";
-  }
-  if (Validator.isEmpty(data.password)) {
-    errors.password = "Password field is required";
-  }
-  if (!Validator.isLength(data.password, {min: 10, max: 50 })) {
-    errors.password = "Password must be between 10 and 50 characters";
   }
 
   return {
