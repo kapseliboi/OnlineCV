131f1b96911a75031d5228c47f3885cb7209ce22
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-10 23:47:42 +0200

    Added a reducer for registering new users

--- client/src/reducers/authReducers.js
+++ client/src/reducers/authReducers.js
@@ -1,11 +1,12 @@
-import { SET_CURRENT_USER, USER_LOADING } from "../actions/types";
+import { SET_CURRENT_USER, USER_LOADING, NEW_USER_REGISTERED } from "../actions/types";
 
 const isEmpty = require("is-empty");
 
 const initialState = {
   isAuthenticated: false,
   user: {},
-  loading: false
+  loading: false,
+  newUser: {}
 };
 
 export default function (state = initialState, action) {
@@ -14,12 +15,18 @@ export default function (state = initialState, action) {
       return {
         ...state, // will be partly overwritten by the additions below
         isAuthenticated: !isEmpty(action.payload),
-        user: action.payload
+        user: action.payload,
+        loading: false
       };
     case USER_LOADING:
       return {
         ...state, // will be partly overwritten by the additions below
         loading: true
+      };
+    case NEW_USER_REGISTERED:
+      return {
+        ...state,
+        newUser: action.payload.newUser
       };
     default:
       return state;
