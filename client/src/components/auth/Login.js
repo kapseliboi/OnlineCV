e1c64d53d468999a879c702a44a25559c1d1d4b7
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-13 23:06:59 +0200

    Created the login component and connected it to redux

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+
+import { loginUser } from "../../actions/authActions";
+
+function mapDispatchToProps(dispatch) {
+  return {
+    loginUser: (userData, history) => dispatch(loginUser(userData, history))
+  };
+}
+
+const mapStateToProps = state => {
+  return {
+    errors: state.loginErrors
+  };
+};
 
 
 class Login extends Component {
-  constructor () {
-    super();
+  constructor (props) {
+    super(props);
     this.state = {
       username: "",
-      password: "",
-      errors: {}
+      password: ""
     };
   }
 
@@ -22,35 +39,55 @@ class Login extends Component {
       username: this.state.username,
       password: this.state.password
     };
-    console.log(userData);
+    this.props.loginUser(userData, this.props.history);
   }
 
   render () {
-    const { errors } = this.state;
+    const errors  = this.props.errors;
+
     return (
-      <div className="container">
+      <div className="container align-items-center">
         <form noValidate onSubmit={this.onSubmit}>
           <div className="form-group">
-            <label htmlFor="username">Username</label>
             <input onChange={this.onChange}
             value={this.state.username}
-            error={errors.username}
-            type="text" className="form-control" id="username"
+            type="text"
+            className={classnames("form-control", {"is-invalid": errors.username})}
+            id="username"
             placeholder="Username"/>
+            <div className={classnames({"invalid-feedback": errors.username})}>
+              {errors.username}
+            </div>
           </div>
+
           <div className="form-group">
-            <label htmlFor="password">Password</label>
             <input onChange={this.onChange}
             value={this.state.password}
             error={errors.password}
-            type="password" className="form-control" id="password"
+            type="password"
+            className={classnames("form-control", {"is-invalid": errors.password})}
+            id="password"
             placeholder="Password"/>
+            <div className={classnames({"invalid-feedback": errors.username})}>
+              {errors.password}
+            </div>
           </div>
-          <button type="submit" className="btn btn-lg btn-primary">Login</button>
+          <div className="form-group">
+            <button type="submit" className="btn btn-lg btn-block btn-primary">Login</button>
+          </div>
+          <div className={classnames({"alert alert-danger": errors.error}, "mt-2")}
+          role="alert">
+            <strong>{errors.error}</strong>
+          </div>
       </form>
     </div>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Login));
