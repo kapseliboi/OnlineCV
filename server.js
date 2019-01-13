2f5ed1be2cd5af52153db1465e2bfae647bbd71b
Akseli Käppi <akseli.kappi@student.tut.fi>
2019-01-14 01:55:46 +0200

    Added production conditions

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 
@@ -39,6 +40,15 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.set("views", "client/views");
+  app.set("view engine", "ejs");
+  app.get("/", (req, res) => {
+    res.render("index");
+  });
+}
 
 const port = process.env.PORT || 5000; // Setting up for possible heroku deployment
 
