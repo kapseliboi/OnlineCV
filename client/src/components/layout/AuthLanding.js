69c0690e0384411a065178a72644726d619284e2
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-13 23:16:07 +0200

    Added a new landing for users who just logged in

---
+++ client/src/components/layout/AuthLanding.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import Navbar from "./Navbar";
+
+class AuthLanding extends Component {
+  render () {
+    return (
+      <Navbar/>
+    );
+  }
+}
+
+export default AuthLanding;
