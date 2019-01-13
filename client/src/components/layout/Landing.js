f7d8190225edf11b9039264d5bb0d66215895f98
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-13 23:08:21 +0200

    Made bootstrap styling better

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -3,15 +3,18 @@ import React, { Component } from "react";
 import Login from "./../auth/Login";
 
 class Landing extends Component {
+  componentDidMount () {
+    console.log("Landing");
+  }
   render () {
     return (
       <div className="container">
         <div>
           <h1 className="display-1 text-center">OnlineCV</h1>
         </div>
-        <div className="mt-5">
-          <h1 className="">Welcome!</h1>
-          <p className="text-justified">Please enter the provided credentials below:</p>
+        <div className="mt-5 align-items-center">
+          <h1 className="text-center">Welcome!</h1>
+          <p className="mt-3 text-center">Please enter the provided credentials below:</p>
         </div>
         <Login />
       </div>
