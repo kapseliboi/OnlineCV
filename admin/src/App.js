d91805b88145bc01bc9a362d9f286fb701abae52
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-15 21:16:30 +0200

    Moved redux Provider from App to index

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,33 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
+import { connect } from "react-redux";
 
-import store from "./store";
+import { getCSRFToken } from "./actions/authActions";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Projects from "./components/layout/Projects";
 import Users from "./components/layout/Users";
-import Register from "./components/auth/Register";
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getCSRFToken: () => dispatch(getCSRFToken())
+  }
+}
 
 class App extends Component {
+  componentDidMount() {
+    this.props.getCSRFToken();
+  }
+
   render() {
     return (
-      <Provider store={store}>
-        <Router>
-          <div className="App">
-            <h1 className="display-1 text-center w-100">OnlineCV</h1>
-            <Navbar />
-            <Switch>
-              <Route exact path="/" component={Landing} />
-              <Route exact path="/projects" component={Projects} />
-              <Route exact path="/users" component={Users} />
-            </Switch>
-          </div>
-        </Router>
-      </Provider>
+      <Router>
+        <div className="App">
+          <h1 className="display-1 text-center w-100">OnlineCV</h1>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/users" component={Users} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default connect(null, mapDispatchToProps)(App);
