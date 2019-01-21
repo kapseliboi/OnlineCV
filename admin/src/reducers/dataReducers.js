5374a12308b88d7a352662c7dd8788f0a6a7aca5
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-21 21:28:56 +0200

    Added reducer functionality to project updating

--- admin/src/reducers/dataReducers.js
+++ admin/src/reducers/dataReducers.js
@@ -1,7 +1,10 @@
-import { GET_PROJECT_DATA } from "../actions/types";
+import { GET_PROJECT_DATA, CREATE_PROJECT, CREATING_PROJECT, GET_START_DATA}
+from "../actions/types";
 
 const initialState = {
-  projects: {}
+  projects: [],
+  creating: false,
+  id: 0
 };
 
 export default function (state = initialState, action) {
@@ -10,6 +13,30 @@ export default function (state = initialState, action) {
       return {
         ...state,
         projects: action.payload
+      };
+    case CREATING_PROJECT:
+      return {
+        ...state,
+        creating: true
+      };
+    case CREATE_PROJECT:
+      return {
+        ...state,
+        creating: false,
+        projects: state.projects.concat({...action.payload, id: state.id}),
+        id: state.id + 1
+      };
+    case GET_START_DATA:
+      var id = state.id;
+      const projects = action.payload.map(project => {
+        project.id = id;
+        id++;
+        return project;
+      });
+      return {
+        ...state,
+        projects: projects,
+        id: id
       };
     default:
       return state;
