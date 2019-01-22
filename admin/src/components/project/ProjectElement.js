8b52a4ccb9543c18a5ab6457e61092f5b88ef01b
Akseli Käppi <akseli.kappi@student.tut.fi>
2019-01-22 17:35:26 +0200

    Transformed from class to stateless component

--- admin/src/components/project/ProjectElement.js
+++ admin/src/components/project/ProjectElement.js
@@ -1,27 +1,36 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-class ProjectElement extends Component {
-  render() {
-    return (
-      <div className="row">
-        <div className="col-lg-6 col border border-dark">
-          <div className="row">
-            <div className="col-8 ">
-              <span className="form-control-plaintext">{this.props.name}</span>
-            </div>
-            <div className="col-4 my-1 d-flex justify-content-center">
-              <button className="btn btn-outline-primary mx-1" type="button">&#8593;</button>
-              <button className="btn btn-outline-primary mx-1" type="button">&#8595;</button>
-              <Link className="btn btn-primary mx-1" to={"/projects/" + this.props.id}>Edit</Link>
-              <button className="btn btn-danger mx-1" type="button">&#10006;</button>
-            </div>
+const ProjectElement = props => {
+  return (
+    <div className="row">
+      <div className="col-lg-6 col border border-dark">
+        <div className="row">
+          <div className="col-8 ">
+            <span className="form-control-plaintext">{props.name}</span>
+          </div>
+          <div className="col-4 my-1 d-flex justify-content-center">
+            <button className="btn btn-outline-primary mx-1" type="button"
+            disabled={props.index===0 || props.moving}
+            onClick={() => props.moveUp(props.index)}>&#8593;</button>
+
+            <button className="btn btn-outline-primary mx-1" type="button"
+            disabled={props.index===props.length-1 || props.moving}
+            onClick={() => props.moveDown(props.index)}>&#8595;</button>
+
+            <Link className="btn btn-primary mx-1" to={"/projects/" + props.id}
+            disabled={props.moving}>Edit</Link>
+
+            <button className="btn btn-danger mx-1" type="button"
+            data-toggle="modal" data-target="#deleteConfirmation"
+            onClick={() => props.setRemoved(props.index)}
+            disabled={props.moving}>&#10006;</button>
           </div>
         </div>
       </div>
+    </div>
 
-    );
-  }
+  );
 }
 
 export default ProjectElement;
