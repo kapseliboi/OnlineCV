4bdb3bddc342ec4fd78873c8b632c1db386e09ae
Akseli Käppi <akseli.kappi@student.tut.fi>
2019-01-22 17:36:10 +0200

    Added project modification reducer cases

--- admin/src/reducers/dataReducers.js
+++ admin/src/reducers/dataReducers.js
@@ -1,32 +1,74 @@
-import { GET_PROJECT_DATA, CREATE_PROJECT, CREATING_PROJECT, GET_START_DATA}
+import { GET_PROJECT_DATA, CREATE_PROJECT, CREATING_PROJECT, GET_START_DATA,
+  MOVE_PROJECT_UP, MOVE_PROJECT_DOWN, DELETE_PROJECT, MOVING_PROJECT }
 from "../actions/types";
 
 const initialState = {
   projects: [],
   creating: false,
+  moving: false,
   id: 0
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case GET_PROJECT_DATA:
+    case GET_PROJECT_DATA: {
       return {
         ...state,
         projects: action.payload
       };
-    case CREATING_PROJECT:
+    }
+    case CREATING_PROJECT: {
       return {
         ...state,
         creating: true
       };
-    case CREATE_PROJECT:
+    }
+    case CREATE_PROJECT: {
       return {
         ...state,
         creating: false,
         projects: state.projects.concat({...action.payload, id: state.id}),
         id: state.id + 1
       };
-    case GET_START_DATA:
+    }
+    case MOVING_PROJECT: {
+      return {
+        ...state,
+        moving: true
+      };
+    }
+    case MOVE_PROJECT_UP: {
+      var newProjects = state.projects.slice();
+      const temp = newProjects[action.payload-1];
+      newProjects[action.payload-1] = newProjects[action.payload];
+      newProjects[action.payload] = temp;
+      return {
+        ...state,
+        projects: newProjects,
+        moving: false
+      };
+    }
+    case MOVE_PROJECT_DOWN: {
+      var newProjects = state.projects.slice();
+      const temp = newProjects[action.payload+1];
+      newProjects[action.payload+1] = newProjects[action.payload];
+      newProjects[action.payload] = temp;
+      return {
+        ...state,
+        projects: newProjects,
+        moving: false
+      };
+    }
+    case DELETE_PROJECT: {
+      var newProjects = state.projects.slice();
+      newProjects.splice(action.payload, 1);
+      return {
+        ...state,
+        projects: newProjects,
+        moving: false
+      };
+    }
+    case GET_START_DATA: {
       var id = state.id;
       const projects = action.payload.map(project => {
         project.id = id;
@@ -38,7 +80,9 @@ export default function (state = initialState, action) {
         projects: projects,
         id: id
       };
-    default:
+    }
+    default: {
       return state;
+    }
   }
 }
