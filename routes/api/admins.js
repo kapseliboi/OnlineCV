6333d5f363e9f2f348ff7b198f283729285b4066
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-24 23:58:59 +0200

    Added removal of files from the server

--- routes/api/admins.js
+++ routes/api/admins.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require("passport");
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
 const Project = require("../../models/Project");
 const parseForm = require("../../utils/parseForm");
@@ -10,10 +11,8 @@ const parseForm = require("../../utils/parseForm");
 router.use(passport.authenticate("jwt", {session: false}));
 
 function fileFilter(req, file, cb) {
-  if (req.user && req.user.isAdmin) {
-    if (file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-      return cb(null, true);
-    }
+  if (file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(null, true);
   }
   return cb(null, false);
 }
@@ -36,9 +35,11 @@ router.post("/projects/create", upload, (req, res) => {
     });
   }
 
+  console.log(req.files);
+
   var countPromise = Project.countDocuments({ owner: req.user.id }).exec();
 
-  const content = parseForm(req.body);
+  const {content, imgURLs} = parseForm(req);
 
   countPromise.then((count) => {
     const newProject = new Project({
@@ -52,10 +53,7 @@ router.post("/projects/create", upload, (req, res) => {
         return res.status(400).json(err);
       }
       else {
-        const imgURLs = req.files.map( img => {
-          return img.path;
-        });
-        res.json({success: true, imgURLS: imgURLs});
+        res.json({success: true, imgURLs: imgURLs});
       }
     });
   }).catch( err => {
@@ -72,17 +70,30 @@ router.post("/projects/update", upload, (req, res) => {
       error: "Not an admin"
     });
   }
-  const content = parseForm(req.body);
+
+  if (req.body.removedImg){
+    if (Array.isArray(req.body.removedImg)){
+      for (var i = 0; i < req.body.removedImg.length; i++){
+        fs.unlink(req.body.removedImg[i], err => {
+          console.log(err);
+        });
+      }
+    }
+    else {
+      fs.unlink(req.body.removedImg, err => {
+        console.log(err);
+      });
+    }
+  }
+
+  const {content, imgURLs} = parseForm(req);
+
   Project.updateOne({position: req.body.index, owner: req.user.id},
     {title: req.body.title, content: content}, (err, raw) => {
       if (err) {
         return res.status(500).json({error: "Something went wrong, please try again."});
       }
-      const imgURLs = req.files.map( img => {
-        return img.path;
-      });
-      console.log("Update succesful");
-      res.json({success: true, imgURLS: imgURLs});
+      res.json({success: true, imgURLs: imgURLs});
     });
 });
 
@@ -111,15 +122,16 @@ router.post("/projects/moveUp", (req, res) => {
       error: "Not an admin"
     });
   }
-  Project.findOne({position: req.body.index}, (err, project) => {
+  Project.findOne({position: req.body.index, owner: req.user.id}, (err, project) => {
     if (err || !project) {
       console.log(err);
       console.log(project);
       return res.status(500).json({error: "Something went wrong."});
     }
-    Project.findOne({position: req.body.index-1}, (err2, project2) => {
+    Project.findOne({position: req.body.index-1, owner: req.user.id},
+      (err2, project2) => {
       if (err2 || !project2) {
-        console.log("Error2");
+        console.log(err2);
         return res.status(500).json({error: "Something went wrong."});
       }
       project2.position = req.body.index;
@@ -138,11 +150,12 @@ router.post("/projects/moveDown", (req, res) => {
       error: "Not an admin"
     });
   }
-  Project.findOne({position: req.body.index}, (err, project) => {
+  Project.findOne({position: req.body.index, owner: req.user.id}, (err, project) => {
     if (err || !project) {
       return res.status(500).json({error: "Something went wrong."});
     }
-    Project.findOne({position: req.body.index+1}, (err2, project2) => {
+    Project.findOne({position: req.body.index+1, owner: req.user.id},
+      (err2, project2) => {
       if (err2 || !project2) {
         return res.status(500).json({error: "Something went wrong."});
       }
@@ -162,14 +175,25 @@ router.post("/projects/delete", (req, res) => {
       error: "Not an admin"
     });
   }
-  Project.deleteOne({position: req.body.index}, (err, result) => {
+  Project.findOne({position: req.body.index, owner: req.user.id}, (err, project) => {
     if (err) {
       return res.status(500).json({error: "Something went wrong."});
     }
-    else if (!result) {
+    else if (!project) {
       return res.status(400).json({error: "Project wasn't found."});
     }
-    Project.updateMany({position: {$gt: req.body.index}}, {$inc: {position: -1}},
+    else {
+      for (var i=0; i < project.content.length; i++){
+        if (project.content[i].type === "image") {
+          fs.unlink(project.content[i].url, err => {
+            console.log(err);
+          });
+        }
+      }
+      project.delete();
+    }
+    Project.updateMany({position: {$gt: req.body.index}, owner: req.user.id},
+      {$inc: {position: -1}},
       (err) => {
       if (err) {
         return res.status(500).json({error: "Something went wrong."});
