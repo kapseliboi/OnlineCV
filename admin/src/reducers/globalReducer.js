5ee1e6c679395788396e0d8801edac3df9bed159
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-25 00:09:29 +0200

    New reducer to handle admin information

---
+++ admin/src/reducers/globalReducer.js
@@ -0,0 +1,18 @@
+import { GET_ADMIN_DATA } from "../actions/types";
+
+const initialState = {
+  user: {}
+};
+
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_ADMIN_DATA:
+      return {
+        ...state,
+        user: action.payload
+      };
+    default:
+      return state;
+  }
+};
