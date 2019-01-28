c7bd7a892f64426551d462b38d2c1b23e343ee15
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-28 21:39:32 +0200

    Implemented the UI of this page

--- admin/src/components/layout/Users.js
+++ admin/src/components/layout/Users.js
@@ -1,12 +1,90 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+import Modal from "../utils/Modal";
+import UserElement from "../users/UserElement";
+import { deleteUser } from "../../actions/applicationActions";
+
+function mapDispatchToProps(dispatch) {
+  return {
+    deleteUser: (username) => dispatch(deleteUser(username))
+  };
+}
+
+const mapStateToProps = state => {
+  return {
+    users: state.application.users,
+    changing: state.application.changing
+  };
+};
 
 
 class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      header: "",
+      toRemove: null
+    };
+  }
+
+  onChange = event => {
+    this.setState({ [event.target.id]: event.target.value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+  };
+
+  setToBeRemoved = (username) => {
+    this.setState({toRemove: username});
+  }
+
+  onRemoval = () => {
+    this.props.deleteUser(this.state.toRemove);
+    this.setState({toRemove: null});
+  }
+
   render () {
     return (
-      <p>Text2</p>
+      <main className="py-md-4 pl-md-5">
+        <Modal title="Confirm remove action" target={this.state.toRemove !== null ?
+        this.state.toRemove : ""}
+        confirm={this.onRemoval} />
+        <h1>Your users</h1>
+        <div className="container-fluid my-2">
+          {this.props.users.map((user, i) =>
+            <UserElement key={user.id}
+            name={user.name} username={user.username}
+            setRemoved={this.setToBeRemoved}
+            changing={this.props.changing} />
+          )}
+        </div>
+        <Link to="/users/add"
+        className="btn btn-lg btn-primary">Register a new user</Link>
+
+        <h1 className="py-md-4">Your targeted user settings</h1>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label htmlFor="inputHeader">Header shown to users</label>
+              <input type="text" className="form-control" id="inputHeader"
+              placeholder="Header" onChange={this.onChange} value={this.state.header}/>
+            </div>
+            <div className="form-group col-md-6">
+              <label htmlFor="headerCurrent">Current header</label>
+              <span className="form-control-plaintext"
+              id="headerCurrent">{this.state.header}</span>
+            </div>
+          </div>
+          <div className="form-row">
+            <button type="submit" className="btn btn-lg btn-primary">Save header</button>
+          </div>
+        </form>
+      </main>
     );
   }
 }
 
-export default Users;
+export default connect(mapStateToProps, mapDispatchToProps)(Users);
