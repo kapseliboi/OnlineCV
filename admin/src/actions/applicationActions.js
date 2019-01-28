50acdc42ca5a2827f24762a8b5811cd9e0f6da2b
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-28 21:42:22 +0200

    Action creator used for events spawned in Applications/Users page

---
+++ admin/src/actions/applicationActions.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+import { USER_REMOVED } from "./types";
+
+export const deleteUser = username => dispatch => {
+  axios.post("/api/admins/deleteUser", {username: username}).then(
+    res => {
+      dispatch({
+        type: USER_REMOVED,
+        payload: username
+      });
+    }
+  ).catch(
+    err => {
+      console.log(err);
+    }
+  );
+}
+
+export const createOrUpdateApplication = (formData, username, history) => {
+
+}
