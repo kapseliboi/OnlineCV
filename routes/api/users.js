979612ceb72261df3db26039e43ba3a94b995d5b
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-28 21:35:24 +0200

    Moved registration to admin route

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,55 +3,11 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
-const generator = require("generate-password");
 
 const keys = require("../../config/keys");
-const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
 
 const User = require("../../models/User");
-
-// Register route
-router.post("/register", passport.authenticate("jwt", { session: false }),
-(req, res) => {
-  if (!req.user.isAdmin) {
-    return res.status(401).json({
-      error: "Not an admin"
-    });
-  }
-  const { errors, isValid } = validateRegisterInput(req.body);
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-  User.findOne({ username: req.body.username }, (err, user) => {
-    if (user) {
-      return res.status(400).json({ username: "Username already exists." });
-    }
-    const newPassword = generator.generate({ length: 40, numbers: true,
-      symbols: true });
-    const newUser = new User({
-      name: req.body.name,
-      username: req.body.username,
-      password: newPassword
-    });
-
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        newUser.save( (err, user) => {
-          if (err) {
-            console.log(err);
-          }
-          else {
-            res.json({ newUser: newUser });
-          }
-        });
-      });
-    });
-  });
-
-});
 
 router.post("/login", (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
