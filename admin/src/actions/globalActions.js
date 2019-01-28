cf0230bece9a2d3737451c93564759977544f7ed
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-28 21:37:30 +0200

    Now dispatches information about users

--- admin/src/actions/globalActions.js
+++ admin/src/actions/globalActions.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-import { GET_PROJECT_DATA, GET_ADMIN_DATA } from "./types";
+import { GET_PROJECT_DATA, GET_ADMIN_DATA, GET_USER_DATA } from "./types";
 
 export const getStartData = () => dispatch => {
   axios.get("/api/admins/startdata", {withCredentials: true}).then(
     res => {
       dispatch({type: GET_PROJECT_DATA, payload: res.data.projects});
       dispatch({type: GET_ADMIN_DATA, payload: res.data.user});
+      dispatch({type: GET_USER_DATA, payload: res.data.users});
     }
   ).catch(
     err => {
