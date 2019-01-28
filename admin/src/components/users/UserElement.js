fa57ad0bc3f59df3a966c169c92ebc60590ecb73
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-28 21:43:22 +0200

    React element that represents a user

---
+++ admin/src/components/users/UserElement.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const UserElement = props => {
+  return (
+    <div className="row">
+      <div className="col-lg-6 col border border-dark">
+        <div className="row">
+          <div className="col-8 ">
+            <span className="form-control-plaintext">{props.username} ({props.name})</span>
+          </div>
+          <div className="col-4 my-1 d-flex justify-content-center">
+
+            <Link className="btn btn-primary mx-1" to={"/users/edit/" + props.username}
+            disabled={props.changing}>Edit</Link>
+
+            <button className="btn btn-danger mx-1" type="button"
+            data-toggle="modal" data-target="#deleteConfirmation"
+            onClick={() => props.setRemoved(props.username)}
+            disabled={props.changing}>&#10006;</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default UserElement;
