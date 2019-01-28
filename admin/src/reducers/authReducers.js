87759b1bc02b0ceb5624e8e2b13f824a27902bf8
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-28 21:40:32 +0200

    Changed reducer to be in line with the current state of program

--- admin/src/reducers/authReducers.js
+++ admin/src/reducers/authReducers.js
@@ -1,23 +1,13 @@
-import { SET_CURRENT_USER, USER_LOADING, NEW_USER_REGISTERED } from "../actions/types";
+import { USER_LOADING, NEW_USER_REGISTERED } from "../actions/types";
 
-const isEmpty = require("is-empty");
 
 const initialState = {
-  isAuthenticated: false,
-  user: {},
   loading: false,
   newUser: {}
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case SET_CURRENT_USER:
-      return {
-        ...state, // will be partly overwritten by the additions below
-        isAuthenticated: !isEmpty(action.payload),
-        user: action.payload,
-        loading: false
-      };
     case USER_LOADING:
       return {
         ...state, // will be partly overwritten by the additions below
@@ -26,7 +16,8 @@ export default function (state = initialState, action) {
     case NEW_USER_REGISTERED:
       return {
         ...state,
-        newUser: action.payload.newUser
+        newUser: action.payload,
+        loading: false
       };
     default:
       return state;
