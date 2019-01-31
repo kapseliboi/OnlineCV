16e3abbcb201ab48e123f4ddb96aefd2dc3ae089
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-31 21:43:14 +0200

    Added support for header names in start data request

--- admin/src/actions/globalActions.js
+++ admin/src/actions/globalActions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-import { GET_PROJECT_DATA, GET_ADMIN_DATA, GET_USER_DATA } from "./types";
+import { GET_PROJECT_DATA, GET_ADMIN_DATA, GET_USER_DATA, PROJECTS_HEADER,
+APPLICATIONS_HEADER } from "./types";
 
 export const getStartData = () => dispatch => {
   axios.get("/api/admins/startdata", {withCredentials: true}).then(
@@ -8,6 +9,21 @@ export const getStartData = () => dispatch => {
       dispatch({type: GET_PROJECT_DATA, payload: res.data.projects});
       dispatch({type: GET_ADMIN_DATA, payload: res.data.user});
       dispatch({type: GET_USER_DATA, payload: res.data.users});
+      if (res.data.headers.cv) {
+
+      }
+      if (res.data.headers.projects) {
+        dispatch({
+          type: PROJECTS_HEADER,
+          payload: res.data.headers.projects
+        });
+      }
+      if (res.data.headers.applications) {
+        dispatch({
+          type: APPLICATIONS_HEADER,
+          payload: res.data.headers.applications
+        });
+      }
     }
   ).catch(
     err => {
