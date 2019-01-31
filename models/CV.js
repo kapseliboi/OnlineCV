afda38f1f1ff687e5bcc83063fb48654af3bcf6f
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-31 21:21:35 +0200

    Added a database model to represent curriculum vitae

---
+++ models/CV.js
@@ -0,0 +1,79 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const CVSchema = new Schema ({
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  phone: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  interests: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+
+  experience: [{
+    title: {
+      type: String
+    },
+    employer: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    start: {
+      type: Date
+    },
+    end: {
+      type: Date
+    }
+  }],
+
+  education: [{
+    title: {
+      type: String
+    },
+    school: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    start: {
+      type: Date
+    },
+    end: {
+      type: Date
+    }
+  }],
+
+  languages: [{
+    name: {
+      type: String
+    },
+    skill: {
+      type: String
+    }
+  }],
+
+  technologies: [{
+    name: {
+      type: String
+    },
+    description: {
+      type: String
+    }
+  }]
+});
+
+module.exports = CV = mongoose.model("CV", CVSchema);
