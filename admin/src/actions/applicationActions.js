57d8f902ddaa679375237323c19f9e7bc37b4c37
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-31 21:34:28 +0200

    Added action creators for updating applications and header

--- admin/src/actions/applicationActions.js
+++ admin/src/actions/applicationActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { USER_REMOVED } from "./types";
+import { USER_REMOVED, APPLICATION_UPDATE, APPLICATIONS_HEADER } from "./types";
 
 export const deleteUser = username => dispatch => {
   axios.post("/api/admins/deleteUser", {username: username}).then(
@@ -20,12 +20,31 @@ export const deleteUser = username => dispatch => {
 export const createOrUpdateApplication = (formData, username, history) => dispatch => {
   axios.post("/api/admins/application", {...formData, username: username}).then(
     res => {
-      console.log("Success");
-      // dispatch({
-      //   type:
-      // })
+      dispatch({
+        type: APPLICATION_UPDATE,
+        payload: {content: [formData.titleMe, formData.textMe, formData.titleYou,
+        formData.textYou], username: username}
+      });
+      history.push("/users");
     }
   ).catch(
+    err => {
+      console.log(err);
+    }
+  );
+};
 
+export const updateApplicationHeader = header => dispatch => {
+  axios.post("/api/admins/headers", {applications: header}).then(
+    res => {
+      dispatch({
+        type: APPLICATIONS_HEADER,
+        payload: header
+      });
+    }
+  ).catch(
+    err => {
+      console.log(err);
+    }
   );
 };
