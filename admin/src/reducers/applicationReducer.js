c46957788dcc15034533c37ae5f577f112336df9
Akseli Käppi <akseli.kappi@gmail.com>
2019-01-31 21:47:44 +0200

    Support for application updating and header updating

--- admin/src/reducers/applicationReducer.js
+++ admin/src/reducers/applicationReducer.js
@@ -1,14 +1,16 @@
-import { GET_USER_DATA, GET_NEW_USER, USER_REMOVED } from "../actions/types";
+import { GET_USER_DATA, GET_NEW_USER, USER_REMOVED, APPLICATION_UPDATE,
+APPLICATIONS_HEADER }
+from "../actions/types";
 
 const initialState = {
   users: [],
-  id: 0
+  id: 0,
+  header: ""
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
     case GET_NEW_USER:
-      console.log("GOT NEW USER");
       return {
         ...state,
         users: state.users.concat({...action.payload, id: state.id}),
@@ -16,11 +18,10 @@ export default function (state = initialState, action) {
       };
     case GET_USER_DATA:
       var id = state.id;
-      const newUsers = action.payload.map((user) => {
-        user.id = id;
+      for (var i=0; i < action.payload.length; i++) {
+        action.payload[i].id = id;
         id++;
-        return user;
-      });
+      }
       return {
         ...state,
         users: action.payload,
@@ -34,6 +35,23 @@ export default function (state = initialState, action) {
       return {
         ...state,
         users: usersAfterDelete
+      };
+    case APPLICATION_UPDATE:
+      var updatedApplicationUsers = state.users.slice();
+      for (var j=0; j < updatedApplicationUsers.length; j++) {
+        if (updatedApplicationUsers[j].username === action.payload.username) {
+          updatedApplicationUsers[j].application = {content: action.payload.content};
+          break;
+        }
+      }
+      return {
+        ...state,
+        users: updatedApplicationUsers
+      };
+    case APPLICATIONS_HEADER:
+      return {
+        ...state,
+        header: action.payload
       };
     default:
       return state;
