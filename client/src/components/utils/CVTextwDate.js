9848059c5b212ff9acaac1df094effe3b085bc10
Akseli Käppi <akseli.kappi@gmail.com>
2019-02-05 01:07:33 +0200

    Simple component with text and date fields to display

---
+++ client/src/components/utils/CVTextwDate.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const CVTextwDate = props => {
+  const start = new Date(props.start);
+  var end;
+  if (props.end) {
+    end = new Date(props.end);
+  }
+  return (
+    <div className="row">
+      <div className="col-3">
+        <div className="ml-3">
+          <p>{start.getFullYear() + "-" + start.getMonth() + " -"}<br/>
+          {end ? end.getFullYear() + "-" + end.getMonth() : "present"}</p>
+        </div>
+      </div>
+      <div className="col-9">
+        <h5><b>{props.title}</b></h5>
+        <p><i>{props.origin}</i></p>
+        <p dangerouslySetInnerHTML={{__html: props.description}}/>
+      </div>
+    </div>
+  );
+};
+
+export default CVTextwDate;
