65495ea722991e928f68989cafd44e2f02676753
Akseli Käppi <akseli.kappi@gmail.com>
2019-02-05 01:06:34 +0200

    Made navbar mobile friendly, at least to some degree

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,66 +1,90 @@
 import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
+import { NavLink, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 import { logoutUser } from "../../actions/authActions";
+import { getStartData } from "../../actions/dataActions";
 
 function mapDispatchToProps(dispatch) {
   return {
-    logoutUser: (history) => dispatch(logoutUser(history))
+    logoutUser: (history) => dispatch(logoutUser(history)),
+    getStartData: (history) => dispatch(getStartData(history))
   };
 }
 
-class Navbar extends Component {
+const mapStateToProps = state => {
+  return {
+    fetched: state.data.fetched,
+    cv: state.data.headers.cv,
+    application: state.data.headers.applications,
+    projects: state.data.headers.projects,
+    username: state.auth.user.username,
+    name: state.auth.user.name
+  };
+};
 
+class Navbar extends Component {
+  constructor (props) {
+    super(props);
+    if (!props.fetched) {
+      props.getStartData(props.history);
+    }
+  }
   onLogoutClicked = () => {
     this.props.logoutUser(this.props.history);
   }
 
   render () {
-    const name = "this.props.name";
-    const username ="asdfasdfasdf";
     return (
-      <header className="navbar navbar-expand navbar-dark bg-dark"
+      <header className="navbar navbar-expand-sm navbar-dark bg-dark sticky-top border-bottom border-info"
       role="navigation">
-      <div className="navbar-nav">
-        <ul className="navbar-nav flex-row">
-          <li className="nav-item active btn-link mx-md-5">
-            <Link className="nav-link active" to="/"><big>CV</big></Link>
-          </li>
-          <li className="nav-item active btn-link mx-md-5">
-            <Link className="nav-link" to="/"><big>Why?</big></Link>
-          </li>
-          <li className="nav-item btn-link active mx-md-5">
-            <Link className="nav-link" to="/"><big>Projects</big></Link>
-          </li>
-        </ul>
-      </div>
+        <button className="navbar-toggler" type="button" data-toggle="collapse"
+        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav">
+            <li className="nav-item mx-md-5">
+              <NavLink className="nav-link" to="/home">
+              <big>{this.props.cv}</big></NavLink>
+            </li>
+            <li className="nav-item mx-md-5">
+              <NavLink className="nav-link" to="/application">
+              <big>{this.props.application}</big></NavLink>
+            </li>
+            <li className="nav-item mx-md-5">
+              <NavLink className="nav-link" to="/projects">
+              <big>{this.props.projects}</big></NavLink>
+            </li>
+          </ul>
+        </div>
 
-      <div className="ml-auto navbar-nav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item active dropdown">
-            <button className="nav-item btn-link btn nav-link dropdown-toggle ml-auto"
-            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-            id="navbarDropdown">
-            {name}
-            </button>
-            <div className="dropdown-menu dropdown-menu-right"
-            aria-labelledby="navbarDropdown">
-              <form>
-                <span className="dropdown-item-text">{username}</span>
-              </form>
-              <div className="dropdown-divider"></div>
-              <button className="dropdown-item btn-link" onClick={this.onLogoutClicked}>
-              Logout
+        <div className="ml-auto navbar-nav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active dropdown">
+              <button className="nav-item btn-link btn nav-link dropdown-toggle ml-auto"
+              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+              id="navbarDropdown">
+              {this.props.name}
               </button>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </header>
+              <div className="dropdown-menu dropdown-menu-right"
+              aria-labelledby="navbarDropdown">
+                <form>
+                  <span className="dropdown-item-text">{this.props.username}</span>
+                </form>
+                <div className="dropdown-divider"></div>
+                <button className="dropdown-item btn-link" onClick={this.onLogoutClicked}>
+                Logout
+                </button>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </header>
     );
   }
 }
 
-export default connect(null, mapDispatchToProps)(withRouter(Navbar));
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Navbar));
