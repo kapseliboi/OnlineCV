79d5632da449b8c62175209a470a1cecf23ddc2c
Akseli Käppi <akseli.kappi@gmail.com>
2019-02-05 01:00:53 +0200

    Added data route for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,50 @@ const keys = require("../../config/keys");
 const validateLoginInput = require("../../validation/login");
 
 const User = require("../../models/User");
+const Headers = require("../../models/Headers");
+const CV = require("../../models/CV");
+const Application = require("../../models/Application");
+const Project = require("../../models/Project");
+
+
+function startData(user, res) {
+  User.findOne({isAdmin: true}, (err, admin) => {
+    if (err) {
+      return res.status(500).json({error: "Something went wrong"});
+    }
+    if (!admin) {
+      return res.status(400).json({error: "No CV available"});
+    }
+    Promise.all([
+      Headers.findOne({owner: admin._id}, "-_id cv applications projects"),
+      CV.findOne({owner: admin._id}, "-_id -__v -owner -technologies._id " +
+      "-languages._id -experience._id -education._id"),
+      Application.findOne({owner: admin._id, target: user.id},
+        "-_id content"),
+      Project.find({owner: admin._id}, "-_id content title").sort("position")
+    ]).then(
+      ([headers, cv, application, projects]) => {
+        return res.json({
+          payload: {
+            username: user.username,
+            name: user.name,
+            isAdmin: user.isAdmin
+          },
+          headers: headers,
+          cv: cv,
+          application: application ? application.content : null,
+          projects: projects,
+          adminName: admin.name
+        });
+      }).catch(
+        err2 => {
+          console.log(err2);
+          return res.status(500).json({error: "Something went wrong"});
+        }
+      );
+
+  });
+}
 
 router.post("/login", (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
@@ -22,7 +66,7 @@ router.post("/login", (req, res) => {
   User.findOne({ username: req.body.username }, (err, user) => {
     if (err) {
       console.log(err);
-      return res.status(401).json({error: "Something went wrong."});
+      return res.status(500).json({error: "Something went wrong."});
     }
     else if (!user) {
       return res.status(401).json({error: "Username or password is incorrect"});
@@ -43,14 +87,21 @@ router.post("/login", (req, res) => {
         expiresIn: "24h"
       },
       (err, token) => {
-        res.cookie("jwt", token, {httpOnly: true});
-        res.json({
-          success: true,
-          payload: payload
-        });
+        res.cookie("jwt", token, {httpOnly: true, maxAge: 86400000});
+        if (user.isAdmin) {
+          console.log("done");
+          return res.json({payload});
+        }
+        return startData(user, res);
+
       });
     });
   });
+});
+
+router.get("/startdata", passport.authenticate("jwt", {session: false}),
+(req, res) => {
+  return startData(req.user, res);
 });
 
 router.post("/logout", passport.authenticate("jwt", { session:false }),
@@ -65,16 +116,6 @@ router.get( "/currentuser", passport.authenticate("jwt", { session: false }),
     username: req.user.username,
     name: req.user.name
   });
-});
-
-router.get("/projectdata", passport.authenticate("jwt", { session: false }),
-(req, res) => {
-  if (!req.user.isAdmin) {
-    return res.status(401).json({
-      error: "Not an admin"
-    });
-  }
-  res.json({ success: true });
 });
 
 
