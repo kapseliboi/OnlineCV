9366221523b38936c3f73b76499e9b3b0076e2fc
Akseli Käppi <akseli.kappi@gmail.com>
2019-02-06 00:17:12 +0200

    Switching to cloudinary image storage forced some changes
    Now also returns cloudinary public_ids

--- utils/parseForm.js
+++ utils/parseForm.js
@@ -3,6 +3,7 @@ module.exports = parseForm = (req) => {
   var imgIndex = 0;
   var textIndex = 0;
   var imgURLs = [];
+  var imgIDs = [];
 
   // If we have multiple images or text objects
   if (Array.isArray(req.body.type)){
@@ -10,45 +11,55 @@ module.exports = parseForm = (req) => {
       if (req.body.type[i] === "image") {
         // If we have multiple images
         if (Array.isArray(req.body.description)){
+          // If image is already stored on server
           if (req.body.url[imgIndex] !== "null") {
             content.push({
               type: req.body.type[i],
               description: req.body.description[imgIndex],
               caption: req.body.caption[imgIndex],
-              url: req.body.url[imgIndex]
+              url: req.body.url[imgIndex],
+              imgID: req.body.imgID[imgIndex]
             });
             imgURLs.push(req.body.url[imgIndex]);
+            imgIDs.push(req.body.imgID[imgIndex]);
           }
           else {
             content.push({
               type: req.body.type[i],
               description: req.body.description[imgIndex],
               caption: req.body.caption[imgIndex],
-              url: req.files[imgIndex].path
+              url: req.files[imgIndex].secure_url,
+              imgID: req.files[imgIndex].public_id
             });
-            imgURLs.push(req.files[imgIndex].path);
+            imgURLs.push(req.files[imgIndex].secure_url);
+            imgIDs.push(req.files[imgIndex].public_id);
           }
           imgIndex++;
         }
         // Single image and at least one text object
         else {
+          // If image is already stored on server
           if (req.body.url !== "null") {
             content.push({
               type: req.body.type[i],
               description: req.body.description,
               caption: req.body.caption,
-              url: req.body.url
+              url: req.body.url,
+              imgID: req.body.imgID
             });
             imgURLs.push(req.body.url);
+            imgIDs.push(req.body.imgID);
           }
           else {
             content.push({
               type: req.body.type[i],
               description: req.body.description,
               caption: req.body.caption,
-              url: req.files[0].path
+              url: req.files[0].secure_url,
+              imgID: req.files[0].public_id
             });
-            imgURLs.push(req.files[0].path);
+            imgURLs.push(req.files[0].secure_url);
+            imgIDs.push(req.files[0].public_id);
           }
         }
       }
@@ -74,23 +85,28 @@ module.exports = parseForm = (req) => {
   else {
     // Only one image or one text
     if (req.body.type === "image") {
+      // If image is already stored on server
       if (req.body.url !== "null") {
         content.push({
           type: req.body.type,
           description: req.body.description,
           caption: req.body.caption,
-          url: req.body.url
+          url: req.body.url,
+          imgID: req.body.imgID
         });
         imgURLs.push(req.body.url);
+        imgIDs.push(req.body.imgID);
       }
       else {
         content.push({
           type: req.body.type,
           description: req.body.description,
           caption: req.body.caption,
-          url: req.files[0].path
+          url: req.files[0].secure_url,
+          imgID: req.files[0].public_id
         });
-        imgURLs.push(req.files[0].path);
+        imgURLs.push(req.files[0].secure_url);
+        imgIDs.push(req.files[0].public_id)
       }
     }
     // Only header
@@ -101,5 +117,5 @@ module.exports = parseForm = (req) => {
       });
     }
   }
-  return {content: content, imgURLs: imgURLs};
+  return {content: content, imgURLs: imgURLs, imgIDs: imgIDs};
 }
