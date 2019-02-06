06dad79398c0664bdaa73b5d42e6a5caa6824997
Akseli Käppi <akseli.kappi@gmail.com>
2019-02-06 02:39:58 +0200

    Redux browser plugin is now enabled only in development

--- client/src/store.js
+++ client/src/store.js
@@ -2,11 +2,25 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk)
-  )
-);
+var store;
+if (process.env.NODE_ENV==="production") {
+  store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunk)
+    )
+  );
 
+}
+else {
+  store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+
+}
 export default store;
